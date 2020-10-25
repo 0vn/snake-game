ebc6c4f573ba60791b9e906dec63c8442db96306
Nipa-Code <60810623+Nipa-Code@users.noreply.github.com>
2020-10-25 18:46:36 +0200

    Add files via upload
    Ascii art example

---
+++ ascii.py
@@ -0,0 +1,12 @@
+import pyfiglet
+
+ascii_banner = pyfiglet.figlet_format("NIPACRYPT")
+print(ascii_banner)
+
+#       _
+# _ __ (_)_ __   __ _
+#| '_ \| | '_ \ / _` |
+#| | | | | |_) | (_| |
+#|_| |_|_| .__/ \__,_|
+#        |_|
+#https://github.com/jorgetstechnology/DeathRansom/blob/master/deathransom.py
